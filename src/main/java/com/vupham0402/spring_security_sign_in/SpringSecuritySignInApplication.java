7f3a98abcf44076a9381aeaceb0d76fd61f122aa
Ken Pham <kpham@eyeprogpo.com>
2023-02-16 16:57:29 -0500

    First commit

---
+++ src/main/java/com/vupham0402/spring_security_sign_in/SpringSecuritySignInApplication.java
@@ -0,0 +1,13 @@
+package com.vupham0402.spring_security_sign_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySignInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vupham0402/spring_security_sign_in/SpringSecuritySignInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vupham0402.spring_security_sign_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySignInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
